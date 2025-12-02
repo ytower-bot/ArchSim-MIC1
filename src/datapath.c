b8668e00deb714402960d7c6c2bfee124dd57f55
jvgusmao06 <jvgusmaosouza@id.uff.br>
2025-12-02 12:00:21 -0300

    Ajeitei o valor dos registradores

--- src/datapath.c
+++ src/datapath.c
@@ -5,13 +5,15 @@
 void init_register_bank(register_bank *rb){
     for(int i = 0; i < 4; i++) rb->AMASK.data[i] = 0;
     for(int i = 4; i < 16; i++) rb->AMASK.data[i] = 1;
+    for(int i = 0; i < 8; i++) rb->SMASK.data[i] = 0;
+    for(int i = 8; i < 16; i++) rb->SMASK.data[i] = 1;
     for(int i = 0; i < 16; i++){
         rb->Rm1.data[i] = 1;
         rb->R0.data[i] = 0;
         rb->SP.data[i] = 0;
     } 
     rb->SP.data[3] = 1;
-    for(int i = 0; i < 15; i++) rb->Rm1.data[i] = 0;
+    for(int i = 0; i < 15; i++) rb->R1.data[i] = 0;
     rb->R1.data[15] = 1;
 }
 
