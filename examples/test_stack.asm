8ae379aba207ffa6e7a8e7c87bfbac5fffdcc627
ytower <ytower@proton.me>
2025-12-05 17:08:03 -0300

    feat: adicionar novos arquivos necessários

---
+++ examples/test_stack.asm
@@ -0,0 +1,14 @@
+START:  
+        LOCO 10
+        PUSH
+        LOCO 20
+        PUSH
+        LOCO 30
+        PUSH
+        POP
+        STOD 102
+        POP
+        STOD 101
+        POP
+        STOD 100
+END:    JUMP END
